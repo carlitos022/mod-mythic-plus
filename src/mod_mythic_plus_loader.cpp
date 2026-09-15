8dcdc30a98c5d625684c1f4f8ea1507faf9e459d
carlitos022 <57599543+carlitos022@users.noreply.github.com>
2026-09-15 18:35:40 -0500

    feat: add module loader

---
+++ src/mod_mythic_plus_loader.cpp
@@ -0,0 +1,11 @@
+/*
+ * mod-mythic-plus
+ * Progressive Mythic Instances for AzerothCore 3.3.5a
+ */
+
+void AddMythicPlusScripts();
+
+void Addmod_mythic_plusScripts()
+{
+    AddMythicPlusScripts();
+}
